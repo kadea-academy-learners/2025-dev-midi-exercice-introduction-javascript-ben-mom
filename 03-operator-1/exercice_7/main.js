aaec9ebb938ef80f054249b7eaf876db14fd7fc0
Benjamin <benmombaya@gmail.com>
2025-06-23 21:04:34 +0100

    nouveau

--- 03-operator-1/exercice_7/main.js
+++ 03-operator-1/exercice_7/main.js
@@ -9,3 +9,20 @@ module.exports = {
   ageGrandPere,
   ageOncle,
 };
+ ageEnfant = 10;
+
+
+ agePere = ageEnfant * 2;
+
+ ageMere = agePere - 5;
+
+
+ ageGrandPere = (ageMere * 2) + (ageEnfant / 2);
+
+
+ageOncle = agePere + 10;
+console.log("Âge de l'enfant :", ageEnfant);
+console.log("Âge du père :", agePere);
+console.log("Âge de la mère :", ageMere);
+console.log("Âge du grand-père :", ageGrandPere);
+console.log("Âge de l'oncle :", ageOncle);
--- 03-operator-1/exercice_8/main.js
+++ 03-operator-1/exercice_8/main.js
@@ -17,3 +17,24 @@ module.exports = {
   totalDepenses,
   reste,
 };
+ salaireMensuel = 500; 
+
+ loyer = salaireMensuel * 0.30;
+
+ nourriture = salaireMensuel * 0.20;
+ transport = salaireMensuel * 0.10;
+ autresDepenses = 50;
+autresDepenses += 25; 
+
+ totalDepenses = loyer + nourriture + transport + autresDepenses;
+
+ reste = salaireMensuel - totalDepenses;
+
+
+console.log("Salaire mensuel :", salaireMensuel);
+console.log("Loyer :", loyer);
+console.log("Nourriture :", nourriture);
+console.log("Transport :", transport);
+console.log("Autres dépenses :", autresDepenses);
+console.log("Total des dépenses :", totalDepenses);
+console.log("Reste :", reste);
